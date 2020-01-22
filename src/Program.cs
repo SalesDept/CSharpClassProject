a3c5dd7c5aced3ac32edcb3730e09ef8d4e1004a
Zachary Sales <z.sales91@gmail.com>
2020-01-21 20:03:47 -0500

    2nd Commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZacharySales.CodeLou.ExerciseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
